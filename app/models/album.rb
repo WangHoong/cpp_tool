0831fb1fc9ee74bff5ae8c6d761e5a9ea2937832
Yeah <zyeah61@gmail.com>
2017-04-10 17:54:08 +0800

    add genre validation

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,8 +1,7 @@
 class Album < ApplicationRecord
   validates :name, presence: true
+  validates_inclusion_of :genre, in: CONSTANTS['genres'].keys, allow_nil: true
+
   scope :recent, -> { order('id DESC') }
-
-
-
 
 end
