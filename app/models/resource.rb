4161d7b28ad2114e0b5b0e608db3303ce1afcd5d
MapleGu <1260302891@qq.com>
2017-04-19 17:01:21 +0800

    delete conflict of resource

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,10 +1,5 @@
 class Resource < ApplicationRecord
   acts_as_paranoid :column => 'deleted', :column_type => 'boolean', :allow_nulls => false
-<<<<<<< HEAD
-  belongs_to :target, polymorphic: true
-=======
   has_many :artist_resources
   has_many :artists, through: :artist_resources
-
->>>>>>> 9fba351e2344dbe8914c251a7ebaac8dca8c4660
 end
