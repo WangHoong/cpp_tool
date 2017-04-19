4273be416a92208e3504ea931c3756f1abf4f523
wangyaoguang <382689420@qq.com>
2017-04-19 17:25:27 +0800

    modify api.md

--- app/models/artist_resource.rb
+++ app/models/artist_resource.rb
@@ -1,4 +1,5 @@
 class ArtistResource < ApplicationRecord
+  acts_as_paranoid :column => 'deleted', :column_type => 'boolean', :allow_nulls => false
   belongs_to :resource
   belongs_to :artist
   accepts_nested_attributes_for :resource, :allow_destroy => true
--- db/migrate/20170418104031_create_artist_resources.rb
+++ db/migrate/20170418104031_create_artist_resources.rb
@@ -4,6 +4,7 @@ class CreateArtistResources < ActiveRecord::Migration[5.0]
       t.integer  "artist_id",                                                                             comment: "艺人ID"
       t.integer  "resource_id",                                                                           comment: "资源ID"
       t.integer  "field",                                                                                 comment: "个人资源区分"
+      t.boolean  "deleted",      default: false,                                                          comment: "true删除,false未删除"
       t.timestamps
     end
   end
