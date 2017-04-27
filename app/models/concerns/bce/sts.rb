46d1aab989adeb8babd115f6ed9e8a6294622ebe
Yeah <zyeah61@gmail.com>
2017-04-27 15:37:53 +0800

    fix sessiointoken

--- app/models/concerns/bce/sts.rb
+++ app/models/concerns/bce/sts.rb
@@ -19,7 +19,7 @@ module Bce
                   accessControlList: [
                     {
                       service: '*',
-                      resource:["#{@bucket}"/*],
+                      resource:["#{@bucket}/*"],
                       region: '*',
                       effect: 'Allow',
                       permission: ['READ', 'WRITE']
