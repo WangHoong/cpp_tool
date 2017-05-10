af48215f15c1fa84cb9e4e9274d3d1678b074f34
hong <wanghong@topdmc.com>
2017-05-10 17:11:02 +0800

    fixed tracks

--- app/controllers/api/v1/tracks_controller.rb
+++ app/controllers/api/v1/tracks_controller.rb
@@ -3,14 +3,13 @@ class Api::V1::TracksController < Api::V1::BaseController
   def index
     page = params.fetch(:page, 1).to_i
     size = params[:size]
-    @tracks = Track.recent.page(page).per(size)
-
+    @tracks = Track.includes(:albums,:artists,:audits).recent.page(page).per(size)
     render json: {tracks: @tracks.as_json(Track.as_list_json_options)}, meta: page_info(@tracks)
   end
 
   def show
     @track = Track.find params[:id]
-    render json: @track
+    render json: {track: @track.as_json(Track.as_list_json_options)}
   end
 
   def create
