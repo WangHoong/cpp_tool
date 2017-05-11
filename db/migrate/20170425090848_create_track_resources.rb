7899acabedae58f9db9bf0391858f60d1bba29cb
hong <wanghong@topdmc.com>
2017-05-11 18:06:39 +0800

    fixed mg

--- db/migrate/20170425090848_create_track_resources.rb
+++ db/migrate/20170425090848_create_track_resources.rb
@@ -1,12 +1,12 @@
 class CreateTrackResources < ActiveRecord::Migration[5.0]
   def change
     create_table :track_resources do |t|
-      t.integer  "resource_id"
       t.integer  "track_id"
-      t.integer  "field"
+      t.string   "file_name"
+      t.integer  "file_type", default: 0
+      t.string   "url"
       t.timestamps
     end
     add_index :track_resources, :track_id
-    add_index :track_resources, :resource_id
   end
 end
