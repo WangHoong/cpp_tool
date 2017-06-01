2c37c134cfa85f8351d7f76f893594aac7b472a6
hong <wanghong@topdmc.com>
2017-06-01 18:35:18 +0800

    fixed tracks

--- app/models/concerns/services/roo_service.rb
+++ app/models/concerns/services/roo_service.rb
@@ -1,8 +1,7 @@
 require 'roo'
 module Services
   class RooService
-
-    HEADER = ["日期", "代理商", "分发渠道", "歌曲id", "ISRC", "UPC ", "歌曲名", "专辑名", "艺人", "业务模式", "单价", "数量", "国家", "报表货币", "结算货币", "汇率"]
+     HEADER = ["日期", "代理商", "分发渠道", "歌曲id", "ISRC", "UPC ", "歌曲名", "专辑名", "艺人", "业务模式", "单价", "数量", "国家", "报表货币", "结算货币", "汇率"]
      def self.validate_revenue(id)
        revenue = Revenue.find(id)
        revenue_file = RevenueFile.find_by(revenue_id: id)
@@ -14,20 +13,22 @@ module Services
           if header != HEADER
             msg = '文件格式不正确'
             result = {data: nil, note: nil, err_message: msg,category: 0,created_at: Time.now}
+            note = RevenueAnalysis.new(result)
+            repository.save(note)
           else
             data = []
             (2..spreadsheet.last_row).each do |i|
-              row = spreadsheet.row(i)
-              track = Track.find_by(title: row[6])
-               if track.blank? 
+               row = spreadsheet.row(i)
+               track = Track.find_by(title: row[6])
+               if track.blank?
                  note = {sheet_name: spreadsheet.sheets.first,line_num: i,revenue_file_id: revenue_file.id,revenue_id: id,
                    dsp_id: revenue.dsp_id,dsp_name: revenue.dsp.try(:name),start_date: revenue.start_time,end_date: revenue.end_time,income: revenue.income}
                  data = {date: row[0],title: row[6],album: row[7],artist: row[8],dsp: row[2]}
                  result = {data: data,note: note, err_message: '歌曲不存在',category: 3,created_at: Time.now}
+                 note = RevenueAnalysis.new(result)
+                 repository.save(note)
                end
             end
-            note = RevenueAnalysis.new(result)
-            repository.save(note)
           end
        end
      end
