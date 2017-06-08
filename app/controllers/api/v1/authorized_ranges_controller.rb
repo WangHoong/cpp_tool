409e4462aaa1e46f91601160cde482163976fbf8
hong <wanghong@topdmc.com>
2017-06-08 19:05:59 +0800

    fixed revenue work

--- app/controllers/api/v1/authorized_ranges_controller.rb
+++ app/controllers/api/v1/authorized_ranges_controller.rb
@@ -4,9 +4,26 @@ class Api::V1::AuthorizedRangesController < Api::V1::BaseController
   def index
     @ranges = AuthorizedRange.all.order(id: :desc)
 
-     Services::RooService.validate_revenue(1)
-
+  #   Services::RooService.validate_revenue(1)
+=begin
+    revenue = Revenue.first
+    response = revenue.analyses_data(:succeed)
+      #time = revenue.analyses_date(:succeed)
+       bucket = {}
      #Services::RooService.seach_revenue(1)
+     response.each do |ele|
+       p ele['note']['provider_id']
+       next if ele['note']['provider_id'].nil?
+       provider_id = ele['note']['provider_id']
+        p provider_id
+       if bucket[provider_id].blank?
+         bucket[provider_id] = []
+       end
+
+       bucket[provider_id] << ele
+     end
+
+=end
 
      render json: {ranges: @ranges}
   end
