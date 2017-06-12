6e1628f96ddafa825c24215facc68aabff561899
liangyali <me@liangyali.com>
2017-06-12 16:00:23 +0800

    add write_audit test

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,32 +1,36 @@
 class Artist < ApplicationRecord
-	include Workflow
-	include ApproveWorkflow
-	audited
-	acts_as_paranoid :column => 'deleted', :column_type => 'boolean', :allow_nulls => false
-	enum gender_type: [:male,:female,:team]
-	validates :name, presence: true
-	belongs_to :country
-	has_many :artist_names
-	# songs resource association
-	has_many :song_resources, -> { where resource_type: 'song' },
-						class_name: 'ArtistResource', dependent: :destroy
-	has_many :songs, :through => :song_resources, class_name: 'Resource', :source => :resource
+  include Workflow
+  include ApproveWorkflow
+  audited
+  acts_as_paranoid :column => 'deleted', :column_type => 'boolean', :allow_nulls => false
+  enum gender_type: [:male, :female, :team]
+  validates :name, presence: true
+  belongs_to :country
+  has_many :artist_names
+  # songs resource association
+  has_many :song_resources, -> {where resource_type: 'song'},
+           class_name: 'ArtistResource', dependent: :destroy
+  has_many :songs, :through => :song_resources, class_name: 'Resource', :source => :resource
 
   # images resource association
   has_many :image_resources, -> {where resource_type: 'image'},
            class_name: 'ArtistResource', dependent: :destroy
   has_many :images, :through => :image_resources, class_name: 'Resource', :source => :resource
 
-	has_and_belongs_to_many :tracks, join_table:'artists_tracks'
+  has_and_belongs_to_many :tracks, join_table: 'artists_tracks'
 
   # artist album association
   has_many :artist_albums, class_name: 'ArtistAlbum'
   has_many :albums, :through => :artist_albums, class_name: 'Album', :source => :album
 
-	accepts_nested_attributes_for :songs, :allow_destroy => true
-	accepts_nested_attributes_for :images, :allow_destroy => true
-	accepts_nested_attributes_for :artist_names, :allow_destroy => true
+  accepts_nested_attributes_for :songs, :allow_destroy => true
+  accepts_nested_attributes_for :images, :allow_destroy => true
+  accepts_nested_attributes_for :artist_names, :allow_destroy => true
 
   scope :recent, -> {order('id DESC')}
+
+  def test
+    write_audit(action: 'test', audited_changes: changes, comment: 'test')
+  end
 
 end
