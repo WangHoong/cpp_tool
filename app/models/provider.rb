7a4806a11346bdf0577dc05afcf7f887ea0531dc
hong <wanghong@topdmc.com>
2017-06-26 15:34:22 +0800

    fixed provider bug

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -18,8 +18,7 @@ class Provider < ApplicationRecord
 	class_attribute :as_list_json_options
 	self.as_list_json_options={
 			only: [:id, :name,:property,:data_type,:contact,:tel,:address,:email,:bank_name,:account_no,:user_name,:cycle,:start_time,:status],
-			include: [audits: {only: [:id,:user_id,:username,:action,:version,:remote_address,:comment,:created_at]}],
-			methods: [:copyright_name]
+			include: [:copyright,audits: {only: [:id,:user_id,:username,:action,:version,:remote_address,:comment,:created_at]}]
 	}
 
 	private
