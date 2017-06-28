40ec01190dabea4ce4288369f8e59e5b3330dbab
hong <wanghong@topdmc.com>
2017-06-28 16:17:20 +0800

    fixed permission group

--- app/controllers/api/v1/permissions_controller.rb
+++ app/controllers/api/v1/permissions_controller.rb
@@ -4,11 +4,11 @@ class Api::V1::PermissionsController < Api::V1::BaseController
 
     @role = Role.find(params[:role_id])
     @permission_ids = @role.permissions.map(&:id)
-    @groups = PermissionGroup.includes(:subclass,:permissions).recent.where(parent_id: 0)
- 
+    @groups = PermissionGroup.recent.rootes
     groups = @groups.map{|g| Hash[{id: g.id,name: g.name,subclass: g.subclass.includes(:permissions).map{|m| Hash[{id: m.id,name: m.name,
-      permissions: m.permissions.map{|n| Hash[{id: n.id,display_name: n.display_name,name: n.name,is_selectd: n.is_selectd(@permission_ids)}]}}]}}]}
+        permissions: m.permissions.map{|n| Hash[{id: n.id,display_name: n.display_name,rule_type: n.rule_type,name: n.name,is_selectd: n.is_selectd(@permission_ids)}]}}]}}]}
     render json: {groups: groups}
+
   end
 
 
