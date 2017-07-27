49b752369c41004c2b4cadc0b5b2bd289ccaf362
MapleGu <1260302891@qq.com>
2017-07-27 11:26:04 +0800

    add language_id and id to multi_languages

--- app/serializers/api/v1/album_serializer.rb
+++ app/serializers/api/v1/album_serializer.rb
@@ -10,11 +10,13 @@ class Api::V1::AlbumSerializer < ActiveModel::Serializer
     :upc,
     :audits,
     :multi_languages
-  def multi_languages
-    @multi_languages = object.multi_languages
-    @multi_languages.map { |artist| {
-      name: artist.name,
-      language_name: artist.language.name
-    }}
-  end
+    def multi_languages
+      @multi_languages = object.multi_languages
+      @multi_languages.map { |album| {
+        name: album.name,
+        id: album.id,
+        language_id: album.language_id,
+        language_name: album.language.name
+      }}
+    end
 end
