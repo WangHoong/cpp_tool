d84a69aa22e9903c05ab9d328529a4fc58500a05
hong <wanghong@topdmc.com>
2017-08-07 15:33:59 +0800

    fixed offline

--- app/models/concerns/offline.rb
+++ app/models/concerns/offline.rb
@@ -1,19 +1,19 @@
 class Offline
-
+#tracks(ids: "123,445")
   def tracks(options)
     result = get_result(options)
     uri = "#{Rails.application.secrets.th_url}/offline/track/assetidrel.json?q_source=#{Rails.application.secrets.q_source}&p_json_dig=#{result}"
     re = RestClient.get(uri)
     JSON.parse(re)
   end
-
+#track_detail(id: "123")
   def track_detail(options)
     result = get_result(options)
     uri =  "#{Rails.application.secrets.th_url}/offline/track/detail.json?q_source=#{Rails.application.secrets.q_source}&p_json_dig=#{result}"
     re = RestClient.get(uri)
     JSON.parse(re)
   end
-
+# getreport(datatype: "fee")
   def getreport(options)
     result = get_result(options)
     uri =  "#{Rails.application.secrets.th_url}/offline/getreport.json?q_source=#{Rails.application.secrets.q_source}&p_json_dig=#{result}"
@@ -21,6 +21,7 @@ class Offline
     JSON.parse(re)
   end
 
+# album_detail(id: "P10000004198")
   def album_detail(options)
     result = get_result(options)
     uri =  "#{Rails.application.secrets.th_url}/offline/album/detail.json?q_source=#{Rails.application.secrets.q_source}&p_json_dig=#{result}"
@@ -32,7 +33,7 @@ class Offline
 
   def get_result(options)
     encrypt = Encrypt.new
-    q_time = Time.now.to_i * 1000
+    q_time = (Time.now.to_f * 1000).to_i
     json = {q_time: q_time}.merge(options).to_json
     public_key = Rails.application.secrets.public_key
     publickey = OpenSSL::PKey::RSA.new(Base64.decode64(public_key))
