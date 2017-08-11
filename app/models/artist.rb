9c459d0a119e33c89624888feced07fb2125f1fd
hong <wanghong@topdmc.com>
2017-08-11 15:09:59 +0800

    fixed artist list

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -51,7 +51,8 @@ class Artist < ApplicationRecord
 	class_attribute :as_list_json_options
 	self.as_list_json_options={
 			only: [:id, :name,:label_id,:label_name,:gender_type,:description,:status,:country_id,:not_through_reason,:website,:created_at,:updated_at,:tracks_count,:albums_count],
-			include: [:albums,:tracks],
+			include: [:albums,:tracks,
+      audits: { only: [:id,:username]}],
       methods: [:country_name]
 	}
 
