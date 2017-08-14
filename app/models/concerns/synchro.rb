e3cb8c505f0feb85f0e927330a6a70cd3d727d04
hong <wanghong@topdmc.com>
2017-08-14 17:40:30 +0800

    fixed synchro

--- app/models/concerns/synchro.rb
+++ app/models/concerns/synchro.rb
@@ -28,10 +28,16 @@ class Synchro
     re = RestClient.get(uri)
     JSON.parse(re)
   end
+  #曲风列表
+  def genre_detail(options={})
+    result = get_result(options={})
+    uri =  "#{Rails.application.secrets.th_url}/offline/genre/detail.json?q_source=#{Rails.application.secrets.q_source}&p_json_dig=#{result}"
+    re = RestClient.get(uri)
+    JSON.parse(re)
+  end
 
 
-
-  def get_result(options)
+  def get_result(options={})
     encrypt = Encrypt.new
     q_time = (Time.now.to_f * 1000).to_i
     json = {q_time: q_time}.merge(options).to_json
