a69e87b35ce9b6099236fa97c7245e282ae74860
MapleGu <1260302891@qq.com>
2017-08-15 16:42:14 +0800

    同步逻辑修改

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,47 +1,58 @@
 class Api::V1::TasksController < Api::V1::BaseController
   # Post /tasks
   def create
-    puts "=============="
-    puts params.to_json
-    puts "=============="
-    @tasks = params.require(:album_ids).map do |id|
+    result = []
+    params.require(:album_ids).each do |id|
       if Task.find_by(:album_id => id, :status => 0)
-        {
+        result << {
           id: id,
           success: false,
           message: "此任务正在等待处理中,请不要重复提交。"
         }
-      else
-        if !Album.find_by(:id => id)
-          {
+        next
+      end
+
+      @album = Album.find_by(:id => id)
+      if (!@album)
+        result << {
+          id: id,
+          success: false,
+          message: "没有此 album"
+        }
+        next
+      end
+
+      if @album.status == "rejected"
+        result << {
+          id: id,
+          success: false,
+          message: "此album审核未通过"
+        }
+        next
+      end
+
+      @task = Task.new(:album_id => id)
+      @album.sync_status = 1
+
+      if @task.save
+        if @album.save
+          result << {
             id: id,
-            success: false,
-            message: "没有此album"
+            success: true
           }
+          next
         else
-          task = Task.new(:album_id => id)
-          if task.save
-            {
-              id: id,
-              success: true
-            }
-          else
-            {
-              id: id,
-              success: false,
-              message: "创建失败请重试，或联系管理员。"
-            }
-          end
+          @task.destroy
         end
       end
+
+      result << {
+        id: id,
+        success: false,
+        message: "创建失败请重试，或联系管理员。"
+      }
     end
-    puts "=============="
-    puts @tasks.to_json
-    puts "=============="
-    if @tasks
-      render json: @tasks
-    else
-      render json: @tasks.errors, status: :unprocessable_entity
-    end
+
+    render json: result
   end
 end
