61aed8c358746d09b29d92ed0e20e6763c58f1c3
MapleGu <1260302891@qq.com>
2017-08-17 19:41:46 +0800

    add something to task

--- app/controllers/api/internal/tasks_controller.rb
+++ app/controllers/api/internal/tasks_controller.rb
@@ -10,9 +10,11 @@ class Api::Internal::TasksController < ApplicationController
   def update
     status = params[:status] || 2
     @task = Task.find(params[:id])
-    @task.album.sync_status = status
-    @task.album.sync_time = DateTime.now
-    if @task.update({ status: status})
+    @task.album.sync_status = params[:status] if params[:status].present?
+    @task.album.sync_time = DateTime.now if params[:status].present?
+    @task.batch_no = params[:batch_no] if params[:batch_no].present?
+    @task.status = params[:status] if params[:status].present?
+    if @task.save)
       render json: @task
     else
       render json: @task.errors, status: :unprocessable_entity
