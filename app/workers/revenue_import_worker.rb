b26f48b628e9ab6333dfeba0dc6d5984c574b9a6
hong <wanghong@topdmc.com>
2017-08-18 10:36:04 +0800

    fixed rev

--- app/workers/revenue_import_worker.rb
+++ app/workers/revenue_import_worker.rb
@@ -1,7 +1,7 @@
 require 'roo'
 class RevenueImportWorker
   include Sidekiq::Worker
-  sidekiq_options queue: :revenue_import, retry: false
+  sidekiq_options queue: :revenue_import, retry: 2
 
   HEADER = ["日期", "代理商", "分发渠道", "歌曲id", "ISRC", "UPC ", "歌曲名", "专辑名", "艺人", "业务模式", "单价", "数量", "国家", "报表货币", "结算货币", "汇率"]
 
