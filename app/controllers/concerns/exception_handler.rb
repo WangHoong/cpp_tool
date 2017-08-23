057e074680582a8b272d08a653b9bb52c95aa7d7
liangyali <me@liangyali.com>
2017-08-24 02:36:49 +0800

    fix bug

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -6,8 +6,8 @@ module ExceptionHandler
 
     rescue_from Exception do |e|
       Raven.capture_exception(e)
-      raise e
-      json_response({err_code: 500, message: e.message}, 500)
+      raise e if Rails.env == 'development'
+      json_response({err_code: 500, message: e}, 500)
     end
   end
 end
---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,11 @@
+if Rails.env=='production'
+
+  Sidekiq.configure_server do |config|
+    config.redis = {url: "redis://#{ENV['REDIS_SERVER']}"}
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = {url: "redis://#{ENV['REDIS_SERVER']}"}
+  end
+
+end
