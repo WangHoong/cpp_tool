7d2968df3036bf1129efc0b7617135d3c1cc734e
hong <wanghong@topdmc.com>
2017-08-28 16:18:05 +0800

    fixed track

--- app/controllers/api/internal/tracks_controller.rb
+++ app/controllers/api/internal/tracks_controller.rb
@@ -2,18 +2,16 @@ class Api::Internal::TracksController < ApplicationController
 
    def index
       ids = params[:ids]
-      if ids.present?
-        synchro = Synchro.new
-        data = synchro.tracks(ids: ids)
-        if data["state"] == true
-          data["result"].each do |obj|
-            @track = Track.where(id: obj['id']).first
-            @track.update(th_id: obj['th_id']) if @track
-          end
+      synchro = Synchro.new
+      data = synchro.tracks(ids: ids)
+      if data["result"] && data["state"] == true
+        data["result"].each do |obj|
+          @track = Track.where(id: obj['id']).first
+          @track.update(th_id: obj['th_id']) if @track
         end
         head :ok
       else
-          render json: {status: 500 }, status: :unprocessable_entity
+        render json: {status: 404 }, status: :unprocessable_entity
       end
    end
 
